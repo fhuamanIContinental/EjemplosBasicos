eccb25e9f56925ee527ea0b78207ec1029e31c2a
Franklin Huaman <franklin.huaman@inetum.com>
2024-05-16 22:37:05 -0500

    subiendo partes iniciales

---
+++ 001/index.js
@@ -0,0 +1,10 @@
+//es la primera función que se ejecuta
+$(document).ready(function () {
+    
+
+
+    $('#section-fotter').load('./footer.html');
+    $('#template-sidenav').load('./sidenav.html');
+    
+});
+
